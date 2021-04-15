fe8711c7c2e74541a39b82a70d73ef879ee7f505
Son Nguyen <idroid3878@gmail.com>
2021-04-15 18:08:09 -0400

    adding json string parse function, tidying files, wire up tableview & tableviewcell

---
+++ Guido/App/GuidomiaAppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  GuidomiaAppDelegate.swift
+//
+//  Created by SonNguyen on 2021-04-15.
+//  Copyright © 2021 sonnguyen. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class GuidomiaAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
--- Guido/Controller/GuidomiaTableViewController.swift
+++ Guido/Controller/GuidomiaTableViewController.swift
@@ -1,10 +1,8 @@
 //
-//  ViewController.swift
-//  mycarapp
+//  GuidomiaTableViewController.swift
 //
 //  Created by SonNguyen on 2021-04-15.
 //  Copyright © 2021 sonnguyen. All rights reserved.
-//
 
 import UIKit
 
@@ -19,13 +17,33 @@ struct Car: Decodable
     let rating: Int
 }
 
-class ViewController:  UITableViewController {
-    
+class GuidomiaTableViewController: UITableViewController {
+
+    var carlist: [Car] = []
+
     var jsonText = """
     [{"consList":["Bad direction"],"customerPrice":120000.0,"make":"Land Rover","marketPrice":125000.0,"model":"Range Rover","prosList":["You can go everywhere","Good sound system"],"rating":3},{"consList":["Sometime explode"],"customerPrice":220000.0,"make":"Alpine","marketPrice":225000.0,"model":"Roadster","prosList":["This car is so fast","Jame Bond would love to steal that car","",""],"rating":4},{"consList":["You can heard the engine over children cry at the back","","You may lose this one if you divorce"],"customerPrice":65000.0,"make":"BMW","marketPrice":55900.0,"model":"3300i","prosList":["Your average business man car","Can bring the family home safely","The city must have"],"rating":5},{"consList":["You may lose a wheel","Expensive maintenance"],"customerPrice":95000.0,"make":"Mercedes Benz","marketPrice":85900.0,"model":"GLE coupe","prosList":[],"rating":2}]
     """
     
-    var carnames = ["Alpine", "LandRover", "Mercedes", "BMW"]
+    func readJsonFromStringVar() {
+        if let data = jsonText.data(using: .utf8) {
+                
+            do {
+                guard let cars = try? JSONDecoder().decode([Car].self, from: data) else {
+                  print("Error: unable to create cars array, verify json  needed!")
+                    return;
+                }
+                
+                carlist = cars
+                
+                for car in carlist {
+                        print("debug car model is \(car.model)")
+                        print("debug car make is \(car.make)")
+                        print("-----")
+                }
+            }
+        }
+    }
     
     func readJsonFromTextFile() -> Void {
         // read file
@@ -36,32 +54,34 @@ class ViewController:  UITableViewController {
             readJsonFromTextFile()
         }
         else {
-            // parse the string above
+            readJsonFromStringVar()
         }
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
         extractJsonInfo(fromFile: false)
     }
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return carnames.count+1
+        return carlist.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "mycustomcell", for: indexPath)
-        //cell.textlabel1="mercedes"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GuidomiaCell", for: indexPath)
+
+        let car_cell = carlist[indexPath.row]
+        cell.textLabel?.text = car_cell.make
+        cell.detailTextLabel?.text = car_cell.model
+        cell.imageView?.image = UIImage(named: car_cell.model)
+        
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("")
     }
 }
-
